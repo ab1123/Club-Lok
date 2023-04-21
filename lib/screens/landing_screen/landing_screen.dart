e8989219b8feddfeb5da2138de915d1ccf00b384
shashank <shashankrawat506@gmail.com>
2023-04-21 11:56:30 +0530

    new screens added

---
+++ lib/screens/landing_screen/landing_screen.dart
@@ -0,0 +1,79 @@
+import 'package:club_lok_test/screens/login/test_login.dart';
+import 'package:club_lok_test/screens/selectrolepage/select_role_page.dart';
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const SelectRolePage()),
+          );
+        },
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(100, 250, 0, 0),
+                  child: Container(
+                    child: const Text(
+                      "CLUB-लोक",
+                      style: TextStyle(
+                          fontSize: 100,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(225, 0, 0, 1)),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(100, 0, 0, 0),
+                  child: Container(
+                    child: const Text(
+                      "A ONE STOP PLACE FOR ALL THE EVENTS IN DIT",
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(400, 50, 0, 0),
+                  child: Container(
+                    child: const Text(
+                      "(Tap on the screen to start..)",
+                      style: TextStyle(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:club_lok_test/screens/landing_screen/landing_screen.dart';
 import 'package:club_lok_test/screens/selectrolepage/select_role_page.dart';
 import 'package:club_lok_test/screens/login/test_login.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,7 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return either Home aur authenticate
-    return const SelectRolePage();
+    return const LandingScreen();
     //return const Authenticate();
   }
 }
